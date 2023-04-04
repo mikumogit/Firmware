f1cb6edb5cfc3ae078686db7e99baad187e81f00
mikumo <mikump@smail.plala.or.jp>
2023-04-04 09:49:00 +0900

    fix key matrix.

--- sidrina_rp/sidrina_rp.h
+++ sidrina_rp/sidrina_rp.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     L00, L01, L02, L03, L04, L05, L06, L07, R00, R01, R02, R03, R04, R05, R06, \
     L10, L11, L12, L13, L14, L15, L16, L17, R10, R11, R12, R13, R14, R15, R16, \
     L20, L21, L22, L23, L24, L25, L26, L27, R20, R21, R22, R23,    R24,   R26, \
-    L30, L31, L32, L33, L34, L35, L36, L37, R30, R31, R32,    R33,   R34, R35, \
+    L30, L31, L32, L33, L34, L35, L36, L37, R30, R31, R32,    R33,   R34, R36, \
     L40, L41, L42, L43,           L46,      R40, R41, R42, R43, R44,      R46  \
   ) \
   { \
@@ -35,6 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { R00,   R01,   R02,   R03,   R04,   R05,   R06 }, \
     { R10,   R11,   R12,   R13,   R14,   R15,   R16 }, \
     { R20,   R21,   R22,   R23,   R24, KC_NO,   R26 }, \
-    { R30,   R31,   R32,   R33,   R34, KC_NO,   R35 }, \
+    { R30,   R31,   R32,   R33,   R34, KC_NO,   R36 }, \
     { R40,   R41,   R42,   R43,   R44, KC_NO,   R46 }  \
   }
